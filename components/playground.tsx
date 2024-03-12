4ba9f0908249c94e7d4d767fde80948a62f40cb2
Aditya Patil <patiladityadr@gmail.com>
2024-03-12 20:37:52 +0530

    created drawable canvas

---
+++ components/playground.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Canvas from "./canvas";
+import Toolbar from "./toolbar";
+
+export default function Playground() {
+  return (
+    <div>
+      <Toolbar />
+      <Canvas />
+    </div>
+  );
+}
